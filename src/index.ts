422de3f1ed97e0618e693ac643afdd9e4a3e073f
Tomer Atar <kenyweb7@gmail.com>
2022-08-18 19:12:28 +0300

    exposed layer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Tool } from './tools/Tool';
 export { default as Brush } from './tools/Brush';
 export { default as Fill } from './tools/Fill';
+export { default as Layer } from './Layer';
 export { default as Editor, EditorOptions } from './Editor';
