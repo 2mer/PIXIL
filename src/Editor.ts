6685a3d6948f3cad64551c1b3096134861b29ba6
Tomer Atar <kenyweb7@gmail.com>
2022-08-18 19:08:44 +0300

    added editor layers

--- src/Editor.ts
+++ src/Editor.ts
@@ -1,11 +1,45 @@
+import { Viewport } from 'pixi-viewport';
 import { Application, IApplicationOptions } from 'pixi.js';
+import Layer from './Layer';
 
 export type EditorOptions = IApplicationOptions;
 
 export default class Editor {
 	app: Application;
+	viewport: Viewport;
+	layers: Layer[] = [];
 
 	constructor(options: EditorOptions) {
 		this.app = new Application(options);
+		this.viewport = new Viewport({
+			screenWidth: window.innerWidth,
+			screenHeight: window.innerHeight,
+			worldWidth: 1000,
+			worldHeight: 1000,
+
+			interaction: this.app.renderer.plugins.interaction,
+		});
+		this.app.stage.addChild(this.viewport);
+
+		this.viewport.drag().pinch().wheel().decelerate();
+	}
+
+	addLayer(layer: Layer) {
+		this.layers.push(layer);
+		this.viewport.addChild(layer.sprite);
+
+		return () => {
+			this.removeLayer(layer);
+		};
+	}
+
+	removeLayer(layer: Layer): void {
+		this.layers.splice(this.layers.indexOf(layer), 1);
+		this.viewport.removeChild(layer.sprite);
+	}
+
+	destroy() {
+		this.app.destroy();
+		this.layers.forEach((layer) => layer.destroy());
 	}
 }
---
+++ src/Layer.ts
@@ -0,0 +1,69 @@
+import { Sprite, Texture } from 'pixi.js';
+
+export default class Layer {
+	canvas: HTMLCanvasElement;
+	texture: Texture;
+	sprite: Sprite;
+	ctx: CanvasRenderingContext2D;
+	destroyed = false;
+	readonly = false;
+	private width: number = 0;
+	private height: number = 0;
+
+	constructor() {
+		this.canvas = document.createElement('canvas');
+
+		this.texture = Texture.from(this.canvas);
+		this.ctx = this.canvas.getContext('2d');
+	}
+
+	resizeTo({ width, height }) {
+		this.resize(width, height);
+	}
+
+	resize(width: number, height: number) {
+		this.width = width;
+		this.height = height;
+
+		this.canvas.width = width;
+		this.canvas.height = height;
+
+		this.sprite.width = width;
+		this.sprite.height = height;
+	}
+
+	loadImage(image: HTMLImageElement) {
+		const handleImageLoad = () => {
+			this.resizeTo(image);
+			this.ctx.drawImage(image, 0, 0);
+			this.texture.update();
+		};
+
+		if (image.complete) {
+			handleImageLoad();
+		} else {
+			image.onload = handleImageLoad;
+		}
+	}
+
+	destroy() {
+		this.texture.destroy();
+		this.canvas = null;
+		this.destroyed = true;
+	}
+
+	static fromUrl(url) {
+		const img = new Image();
+		img.src = url;
+
+		return Layer.fromImage(img);
+	}
+
+	static fromImage(image: HTMLImageElement) {
+		const layer = new Layer();
+
+		layer.loadImage(image);
+
+		return layer;
+	}
+}
